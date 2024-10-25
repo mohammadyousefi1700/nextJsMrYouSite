a84ce3eb519d20800e8e28453e147be5098f9093
m.yousefi <mohammad.yousefi.1700@gmail.com>
2024-10-25 15:54:29 +0330

    vip

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default async function Home({
       Query.offset(
         Number(
           searchParams.currentPage === undefined || null
-            ? 1
+            ? 0
             : searchParams.currentPage
         )
       ),
